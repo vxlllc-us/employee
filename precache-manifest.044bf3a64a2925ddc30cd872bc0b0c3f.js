9ae334f594f908fc7e58c421f939d0d137cfc61b
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-04-29 16:42:29 +0530

    Updates

---
+++ precache-manifest.044bf3a64a2925ddc30cd872bc0b0c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0fd8bc6dc2fbb611b1cdb535d5850b",
+    "url": "/employee/index.html"
+  },
+  {
+    "revision": "fc2a86d6525844caa7b4",
+    "url": "/employee/static/css/main.6681eae7.chunk.css"
+  },
+  {
+    "revision": "7ad5ac5e614034ffe2ed",
+    "url": "/employee/static/js/2.ae43e1e7.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/employee/static/js/2.ae43e1e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2a86d6525844caa7b4",
+    "url": "/employee/static/js/main.9b466295.chunk.js"
+  },
+  {
+    "revision": "b263d4d060ade8621f5f",
+    "url": "/employee/static/js/runtime-main.2a14b779.js"
+  },
+  {
+    "revision": "0b50e4b8b718f36c8e2d9fd12af9bd9a",
+    "url": "/employee/static/media/google.0b50e4b8.png"
+  }
+]);
